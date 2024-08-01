0264912665e27fffb1f46764b5067b9a5f80e58b
Matheus Pedro da Silva Sousa <47851260+MatheusPedroSS@users.noreply.github.com>
2024-08-01 02:31:42 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mensagem para o Matheus do futuro... 1% melhor que ontem, 1% pior que amanhã!!!</h1>
+    return <h1>Mensagem para o Matheus do futuro... 1% melhor que ontem, 1% pior que amanhã!!! :-)</h1>
 }
 
 export default Home;
